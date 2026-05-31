e48693983cafc321100bb39945cd189ff6937279
noofkamal20 <novk11884@gmail.com>
2026-05-31 23:04:57 +0300

    blue

--- roles/buyer/dashboard.php
+++ roles/buyer/dashboard.php
@@ -1,9 +1,11 @@
 <?php
-include_once '../../includes/auth_guard.php';
-requireRole(['Buyer']);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 echo "Buyer Dashboard";
 ?>
-<div style="display:flex; justify-content:flex-end; padding:20px;">
+<div style="display:flex; justify-content:flex-end; background-color: blue; padding:20px;">
     <a href="/NextPickStore/auth/logout.php" class="logout-btn">Logout</a>
 </div>
